6ee4fa5036a1233ed7695b54ffb1b5f0c893b851
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-01-31 15:55:51 -0300

    refactor: recreate the project with an updated version of vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') }
-    ]
-  }
+	plugins: [react()]
 });
